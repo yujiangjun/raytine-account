cb6e3c1aba9c163ebfc1a1a5fca0fd639dcd53c5
yujiangjun <18512106361>
2022-09-26 23:01:16 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version("2.7.4")
+    id("io.spring.dependency-management") version ("1.0.14.RELEASE")
+}
+
+group = "com.yujiangjun"
+version = "1.0-SNAPSHOT"
+
+configurations{
+    compileOnly{
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+configurations.all{
+    exclude("org.springframework.boot","spring-boot-starter-logging")
+//    exclude(null,"logback-classic")
+//    exclude(null,"logback-core")
+}
+repositories {
+    maven{
+        setUrl("https://maven.aliyun.com/repository/public/")
+    }
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.lmax:disruptor:3.4.4")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-log4j2")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("mysql:mysql-connector-java:8.0.30")
+    implementation("com.baomidou:mybatis-plus-boot-starter:3.5.2")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
